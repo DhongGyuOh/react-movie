a6d0c185db7ef0ce89dc8b69d9043d826e7e8e68
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-06-07 16:49:38 +0900

    React JS movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+import React from "react";
 
 function App() {
+  return;
+}
+function Button() {
+  const [counter, setCounter] = React.useState();
+  const counting = () => {
+    setCounter(+1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className={styles.btn} text="good" onClick={counting}>
+      Good button
+    </button>
   );
 }
+Button.prototype = {
+  text: PropTypes.string.isRequired,
+};
 
 export default App;
