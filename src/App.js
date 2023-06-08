0025b0c945ed9e53a45ffcd13fcbea4cf65ae427
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-06-08 15:30:19 +0900

    ToDoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return;
-}
-function Button() {
-  const [counter, setCounter] = React.useState();
-  const counting = () => {
-    setCounter(+1);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
   };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+
+  const onClickDelete = (index) => {
+    setToDos((currentArray) => {
+      const updatedArray = [...currentArray];
+      updatedArray.splice(index, 1);
+      return updatedArray;
+    });
+  };
+
   return (
-    <button className={styles.btn} text="good" onClick={counting}>
-      Good button
-    </button>
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <h1>To Do List({toDos.length})</h1>
+          <input
+            onChange={onChange}
+            type="text"
+            placeholder="Insert To Do"
+            value={toDo}
+          />
+          <button>Add To Do</button>
+        </div>
+      </form>
+      <ol>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => onClickDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
-Button.prototype = {
-  text: PropTypes.string.isRequired,
-};
 
 export default App;
